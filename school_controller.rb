df0d3f9366d92180d0f22ce3a0ce2f3c6c055d11
aconstantinou123 <a_constantinou@hotmail.co.uk>
2017-11-23 13:54:35 +0000

    options menu now loops houses

--- school_controller.rb
+++ school_controller.rb
@@ -2,6 +2,7 @@ require('sinatra')
 require('sinatra/contrib/all')
 require('pry-byebug')
 require_relative('models/student.rb')
+require_relative('models/house.rb')
 
 get '/students' do
   @students = Student.list_all()
@@ -9,6 +10,7 @@ get '/students' do
 end
 
 get '/students/new' do
+  @houses = House.list_all()
   erb (:new)
 end
 
