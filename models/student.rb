17e5570c144f4e544ca7799bc7b80a154e2373c1
aconstantinou123 <a_constantinou@hotmail.co.uk>
2017-11-23 13:01:57 +0000

    new students can be added

--- models/student.rb
+++ models/student.rb
@@ -2,6 +2,9 @@ require_relative('../db/sql_runner.rb')
 
 class Student
 
+  attr_reader :first_name, :second_name, :house, :age
+
+
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @first_name = options['first_name']
